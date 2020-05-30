7db6e777de7a7303c1398954c37314131554acd4
suryan535 <suryan535@gmail.com>
2020-05-30 19:10:38 +0530

    Updates

---
+++ precache-manifest.9138881e1d97be41b9cd677af4c43655.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a8344b68452d2bbd0dfce2aa3355b07",
+    "url": "/Burger/index.html"
+  },
+  {
+    "revision": "ed77f93304af628b0ec3",
+    "url": "/Burger/static/css/main.e9472430.chunk.css"
+  },
+  {
+    "revision": "b011e5638668a4a5ea48",
+    "url": "/Burger/static/js/2.ee5b0982.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Burger/static/js/2.ee5b0982.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed77f93304af628b0ec3",
+    "url": "/Burger/static/js/main.ed880d1b.chunk.js"
+  },
+  {
+    "revision": "47c7dd09960d292210f7",
+    "url": "/Burger/static/js/runtime-main.6b8ba36d.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/Burger/static/media/127 burger-logo.b8503d26.png"
+  }
+]);
